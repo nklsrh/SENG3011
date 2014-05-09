81efb3fdd1f3a10e07220604e37b0fc75b47a3a9
Nikhil S <nklsrh@gmail.com>
2014-05-09 17:54:17 +1000

    New GUI is work in progress. Import a CSV and watch it load into the GUI. Thats it...

---
+++ GUI/MSM_Trader/MSM_Trader/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace MSM_Trader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabs = new System.Windows.Forms.TabControl();
+            this.tabInput = new System.Windows.Forms.TabPage();
+            this.dataInput = new System.Windows.Forms.DataGridView();
+            this.tabOutput = new System.Windows.Forms.TabPage();
+            this.dataOutput = new System.Windows.Forms.DataGridView();
+            this.statusBar = new System.Windows.Forms.StatusStrip();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstStrategy = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtVal_Threshold = new System.Windows.Forms.TextBox();
+            this.txtVal_Window = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chkAutoImport = new System.Windows.Forms.CheckBox();
+            this.tabs.SuspendLayout();
+            this.tabInput.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataInput)).BeginInit();
+            this.tabOutput.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataOutput)).BeginInit();
+            this.statusBar.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabs
+            // 
+            this.tabs.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabs.Controls.Add(this.tabInput);
+            this.tabs.Controls.Add(this.tabOutput);
+            this.tabs.Location = new System.Drawing.Point(-1, 79);
+            this.tabs.Name = "tabs";
+            this.tabs.SelectedIndex = 0;
+            this.tabs.Size = new System.Drawing.Size(701, 446);
+            this.tabs.TabIndex = 1;
+            // 
+            // tabInput
+            // 
+            this.tabInput.Controls.Add(this.dataInput);
+            this.tabInput.Location = new System.Drawing.Point(4, 22);
+            this.tabInput.Name = "tabInput";
+            this.tabInput.Padding = new System.Windows.Forms.Padding(3);
+            this.tabInput.Size = new System.Drawing.Size(693, 420);
+            this.tabInput.TabIndex = 0;
+            this.tabInput.Text = "Input";
+            this.tabInput.UseVisualStyleBackColor = true;
+            // 
+            // dataInput
+            // 
+            this.dataInput.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataInput.Location = new System.Drawing.Point(3, 3);
+            this.dataInput.Name = "dataInput";
+            this.dataInput.RowTemplate.Height = 24;
+            this.dataInput.Size = new System.Drawing.Size(687, 414);
+            this.dataInput.TabIndex = 0;
+            // 
+            // tabOutput
+            // 
+            this.tabOutput.Controls.Add(this.dataOutput);
+            this.tabOutput.Location = new System.Drawing.Point(4, 22);
+            this.tabOutput.Name = "tabOutput";
+            this.tabOutput.Padding = new System.Windows.Forms.Padding(3);
+            this.tabOutput.Size = new System.Drawing.Size(581, 414);
+            this.tabOutput.TabIndex = 1;
+            this.tabOutput.Text = "Output";
+            this.tabOutput.UseVisualStyleBackColor = true;
+            // 
+            // dataOutput
+            // 
+            this.dataOutput.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataOutput.Location = new System.Drawing.Point(3, 3);
+            this.dataOutput.Name = "dataOutput";
+            this.dataOutput.RowTemplate.Height = 24;
+            this.dataOutput.Size = new System.Drawing.Size(575, 408);
+            this.dataOutput.TabIndex = 1;
+            // 
+            // statusBar
+            // 
+            this.statusBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.statusBar.Location = new System.Drawing.Point(0, 526);
+            this.statusBar.Name = "statusBar";
+            this.statusBar.Size = new System.Drawing.Size(699, 22);
+            this.statusBar.TabIndex = 2;
+            this.statusBar.Text = "statusStrip1";
+            // 
+            // btnImport
+            // 
+            this.btnImport.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnImport.Location = new System.Drawing.Point(7, 3);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(100, 33);
+            this.btnImport.TabIndex = 0;
+            this.btnImport.Text = "Import";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnExport.Location = new System.Drawing.Point(606, 3);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(65, 56);
+            this.btnExport.TabIndex = 1;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnRun.Location = new System.Drawing.Point(478, 3);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(74, 56);
+            this.btnRun.TabIndex = 2;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(177, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Find Strategy";
+            // 
+            // lstStrategy
+            // 
+            this.lstStrategy.Font = new System.Drawing.Font("Segoe WP", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstStrategy.FormattingEnabled = true;
+            this.lstStrategy.ItemHeight = 17;
+            this.lstStrategy.Items.AddRange(new object[] {
+            "Momentum"});
+            this.lstStrategy.Location = new System.Drawing.Point(174, 31);
+            this.lstStrategy.Name = "lstStrategy";
+            this.lstStrategy.Size = new System.Drawing.Size(77, 21);
+            this.lstStrategy.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Controls.Add(this.chkAutoImport);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.txtVal_Threshold);
+            this.panel1.Controls.Add(this.txtVal_Window);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.lstStrategy);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnRun);
+            this.panel1.Controls.Add(this.btnExport);
+            this.panel1.Controls.Add(this.btnImport);
+            this.panel1.Location = new System.Drawing.Point(-1, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(701, 62);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtVal_Threshold
+            // 
+            this.txtVal_Threshold.Location = new System.Drawing.Point(385, 35);
+            this.txtVal_Threshold.Name = "txtVal_Threshold";
+            this.txtVal_Threshold.Size = new System.Drawing.Size(87, 22);
+            this.txtVal_Threshold.TabIndex = 8;
+            this.txtVal_Threshold.Text = "0.001";
+            // 
+            // txtVal_Window
+            // 
+            this.txtVal_Window.Location = new System.Drawing.Point(385, 7);
+            this.txtVal_Window.Name = "txtVal_Window";
+            this.txtVal_Window.Size = new System.Drawing.Size(87, 22);
+            this.txtVal_Window.TabIndex = 7;
+            this.txtVal_Window.Text = "3";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(324, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Threshold";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(333, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Window";
+            // 
+            // openFile
+            // 
+            this.openFile.DefaultExt = "csv";
+            this.openFile.FileName = "inputFile";
+            this.openFile.Filter = "Sirca CSV|*.csv";
+            this.openFile.FileOk += new System.ComponentModel.CancelEventHandler(this.openFile_FileOk);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(163, 17);
+            this.lblStatus.Text = "Ready, waiting for file import.";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(257, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(61, 56);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Find";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // chkAutoImport
+            // 
+            this.chkAutoImport.AutoSize = true;
+            this.chkAutoImport.Location = new System.Drawing.Point(8, 40);
+            this.chkAutoImport.Name = "chkAutoImport";
+            this.chkAutoImport.Size = new System.Drawing.Size(99, 17);
+            this.chkAutoImport.TabIndex = 10;
+            this.chkAutoImport.Text = "Import on start";
+            this.chkAutoImport.UseVisualStyleBackColor = true;
+            this.chkAutoImport.CheckedChanged += new System.EventHandler(this.chkAutoImport_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 548);
+            this.Controls.Add(this.statusBar);
+            this.Controls.Add(this.tabs);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabs.ResumeLayout(false);
+            this.tabInput.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataInput)).EndInit();
+            this.tabOutput.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataOutput)).EndInit();
+            this.statusBar.ResumeLayout(false);
+            this.statusBar.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabs;
+        private System.Windows.Forms.TabPage tabInput;
+        private System.Windows.Forms.DataGridView dataInput;
+        private System.Windows.Forms.TabPage tabOutput;
+        private System.Windows.Forms.DataGridView dataOutput;
+        private System.Windows.Forms.StatusStrip statusBar;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstStrategy;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtVal_Threshold;
+        private System.Windows.Forms.TextBox txtVal_Window;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox chkAutoImport;
+    }
+}
+
---
+++ GUI/MSM_Trader/MSM_Trader/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MSM_Trader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string inputFileName;
+        public string inputFileURL;
+        
+        public string executableURL;
+        public string executableArguments;
+
+        public string threshold;
+        public string window;
+        
+        public string settingsFile = System.IO.Directory.GetCurrentDirectory() + "\\TraderSettings.cfg";
+
+        public bool importOnStart = true;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            openFile.InitialDirectory = System.IO.Directory.GetCurrentDirectory();
+           
+            if (LoadSettingsFile())
+            {
+                openFile.InitialDirectory = inputFileName;
+                if (importOnStart)
+                {
+                    OpenCSV(dataInput, inputFileName);
+                }
+            }
+            else
+            {
+                executableURL = "";
+                executableArguments = "";
+                threshold = "0.01";
+                window = "3";
+                WriteSettingsFile(executableURL, inputFileName, threshold, window);
+            }
+        }
+
+        public bool LoadSettingsFile()
+        {
+            if (System.IO.File.Exists(settingsFile))
+            {
+                try
+                {
+                    System.IO.StreamReader fileReader = new System.IO.StreamReader(settingsFile, false);
+
+                    lblStatus.Text = "Reading settings file.";
+                    int lineNumber = 0;
+                    //Reading Data
+                    while (fileReader.Peek() != -1)
+                    {
+                        string fileRow = fileReader.ReadLine();
+                        string[] fileDataField = fileRow.Split('=');
+
+                        if (fileDataField.Length > 0)
+                        {
+                            if (fileDataField[0] == "tradingStrategyExecutable")
+                            {
+                                executableURL = fileDataField[1];
+                            }
+                            else if (fileDataField[0] == "inputCSVFile")
+                            {
+                                inputFileURL = fileDataField[1];
+                                inputFileName = inputFileURL.Split('/')[inputFileURL.Split('/').Length - 1];
+                            }
+                            else if (fileDataField[0] == "threshold")
+                            {
+                                threshold = fileDataField[1];
+                                txtVal_Threshold.Text = threshold;
+                            }
+                            else if (fileDataField[0] == "window")
+                            {
+                                window = fileDataField[1];
+                                txtVal_Window.Text = window;
+                            }
+                            else if (fileDataField[0] == "importOnStart")
+                            {
+                                importOnStart = fileDataField[1] == "True";
+                                chkAutoImport.Checked = importOnStart;
+                            }
+                        }
+                        lineNumber++;
+                    }
+                    fileReader.Dispose();
+                    fileReader.Close();
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool WriteSettingsFile (string exe="", string input="", string threshold="0.01", string window="3")
+        {
+            try
+            {
+                System.IO.StreamWriter fileWriter = new System.IO.StreamWriter(settingsFile, false);
+                fileWriter.WriteLine("tradingStrategyExecutable=" + exe);
+                fileWriter.WriteLine("inputCSVFile=" + input);
+                fileWriter.WriteLine("threshold=" + threshold);
+                fileWriter.WriteLine("window=" + window);
+                fileWriter.WriteLine("importOnStart=" + importOnStart);
+                fileWriter.Dispose();
+                fileWriter.Close();
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            openFile.ShowDialog();
+        }
+
+        private void openFile_FileOk(object sender, CancelEventArgs e)
+        {
+            inputFileName = openFile.SafeFileName;
+            inputFileURL = openFile.FileName;
+
+            lblStatus.Text = "Input file found. Loading...";
+
+            WriteSettingsFile(executableURL, inputFileURL, threshold, window);
+            SetInputFile();
+        }
+
+
+        public void SetInputFile()
+        {
+            this.Text = inputFileName;
+
+            OpenCSV(dataInput, inputFileURL);
+        }
+
+        public void OpenCSV(DataGridView dataInput, string csvPath)
+        {
+            dataInput.Rows.Clear();
+            try
+            {
+                if (System.IO.File.Exists(csvPath))
+                {
+                    System.IO.StreamReader fileReader = new System.IO.StreamReader(csvPath, false);
+
+                    //Reading Data
+                    while (fileReader.Peek() != -1)
+                    {
+                        string fileRow = fileReader.ReadLine();
+                        string[] fileDataField = fileRow.Split(',');
+                        //Console.WriteLine(fileDataField[0]);
+                        int i = 0;
+                        while (dataInput.Columns.Count < fileDataField.Length)
+                        {
+                            dataInput.Columns.Add(fileDataField[i], fileDataField[i]);
+                            i++;
+                        }
+                        if (i == 0) // skipping first line which is header
+                        {
+                            dataInput.Rows.Add(fileDataField);
+                        }
+                    }
+                    fileReader.Dispose();
+                    fileReader.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Couldn't find CSV at " + csvPath);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error:" + ex.ToString());
+            }
+            finally 
+            {
+                lblStatus.Text = "Ready.";
+            }
+        }
+
+        private void chkAutoImport_CheckedChanged(object sender, EventArgs e)
+        {
+            importOnStart = chkAutoImport.Checked;
+            WriteSettingsFile(executableURL, inputFileURL, threshold, window);
+        }
+
+    }
+}
